0cd5e1c4d0e110fa5996fda93ed038fd2528081a
premSuperdev <108412360+premSuperdev@users.noreply.github.com>
2023-04-13 03:16:54 +0700

    save change

--- app/src/main/java/com/example/easytax/MainActivity.java
+++ app/src/main/java/com/example/easytax/MainActivity.java
@@ -3,16 +3,35 @@ package com.example.easytax;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-
-        setContentView(R.layout.activity_main);
+        Intent intent =null;
+        SharedPreferences sharedPreferences = getSharedPreferences("EasyTax", Context.MODE_PRIVATE);
+        if(!sharedPreferences.contains("username")){
+            String value = sharedPreferences.getString("username", null);
+            if (value != null){
+                intent = new Intent(MainActivity.this, SelectorActivity.class);
+            }
+            else{
+                intent = new Intent(MainActivity.this, UserNameActivity.class);
+            }
+        }
+        else{
+            intent = new Intent(MainActivity.this, SelectorActivity.class);
+        }
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/easytax/SelectorActivity.java
+++ app/src/main/java/com/example/easytax/SelectorActivity.java
@@ -2,7 +2,11 @@ package com.example.easytax;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SelectorActivity extends AppCompatActivity {
 
@@ -10,5 +14,8 @@ public class SelectorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selector);
+        TextView username = findViewById(R.id.nameTv);
+        SharedPreferences sharedPreferences = getSharedPreferences("EasyTax", Context.MODE_PRIVATE);
+        username.setText(sharedPreferences.getAll().get("username").toString());
     }
 }
